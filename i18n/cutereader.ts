04b7e01dfa65b7a1a4677dc3ab0d95bdf69a3d97
Ruslan Nigmatullin <euroelessar@yandex.ru>
2014-01-08 15:32:31 +0400

    Added localization source file

---
+++ i18n/cutereader.ts
@@ -0,0 +1,479 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>BookInfo</name>
+    <message>
+        <location filename="../lib/bookinfo.cpp" line="29"/>
+        <source>Read</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/bookinfo.cpp" line="43"/>
+        <source>Author: %1&lt;br&gt;
+Genres: %2&lt;br&gt;
+Series: %3&lt;br&gt;
+File: %4</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BookPageItem</name>
+    <message>
+        <location filename="../sailfish/qml/pages/BookPageItem.qml" line="144"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/BookPageItem.qml" line="148"/>
+        <source>Library</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/BookPageItem.qml" line="153"/>
+        <source>Contents</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/BookPageItem.qml" line="159"/>
+        <source>About the book</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BookRootModel</name>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="8"/>
+        <source>Authors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="8"/>
+        <source>Books categorized by authors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="8"/>
+        <location filename="../lib/models/bookrootmodel.cpp" line="13"/>
+        <location filename="../lib/models/bookrootmodel.cpp" line="18"/>
+        <location filename="../lib/models/bookrootmodel.cpp" line="23"/>
+        <location filename="../lib/models/bookrootmodel.cpp" line="27"/>
+        <source>Local</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="13"/>
+        <source>Genres</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="13"/>
+        <source>Books categorized by genres</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="18"/>
+        <source>Series</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="18"/>
+        <source>Books categorized by series</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="23"/>
+        <source>Titles</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="23"/>
+        <source>Books categorized by titles</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="27"/>
+        <source>Recent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="27"/>
+        <source>Recently opened books</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="31"/>
+        <source>Opds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="31"/>
+        <source>Network library</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/models/bookrootmodel.cpp" line="31"/>
+        <source>Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BookSettingsSource</name>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="20"/>
+        <source>&lt;Don&apos;t change&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="67"/>
+        <location filename="../lib/booksettingssource.cpp" line="79"/>
+        <source>Normal style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="68"/>
+        <source>Italic style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="80"/>
+        <source>Bold style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="91"/>
+        <location filename="../lib/booksettingssource.cpp" line="104"/>
+        <source>Turn off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="92"/>
+        <location filename="../lib/booksettingssource.cpp" line="103"/>
+        <source>Turn on</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="115"/>
+        <source>Normal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="116"/>
+        <source>Above the base line</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="117"/>
+        <source>Below the base line</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="128"/>
+        <source>Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="129"/>
+        <source>Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="130"/>
+        <source>Center</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="131"/>
+        <source>Justify</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="188"/>
+        <source>Font family</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="189"/>
+        <source>Font size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="190"/>
+        <source>Bold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="191"/>
+        <source>Italic</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="194"/>
+        <source>Strike out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="195"/>
+        <source>Underline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="196"/>
+        <source>Vertical alignment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="200"/>
+        <source>Alignment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="204"/>
+        <source>Top margin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="205"/>
+        <source>Bottom margin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="206"/>
+        <source>Left margin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="207"/>
+        <source>Right margin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="208"/>
+        <source>First line indentation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="226"/>
+        <source>Standard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="227"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="228"/>
+        <source>Subtitle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="229"/>
+        <source>Epigraph</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="230"/>
+        <source>Poem</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="231"/>
+        <source>Stanza</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="232"/>
+        <source>Cite</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="233"/>
+        <source>Text author</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="234"/>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="236"/>
+        <source>Strong</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="237"/>
+        <source>Emphasis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="238"/>
+        <source>Strike through</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="239"/>
+        <source>Sub</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="240"/>
+        <source>Sup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="241"/>
+        <source>Internal anchor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="242"/>
+        <source>Note anchor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="243"/>
+        <source>External anchor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/booksettingssource.cpp" line="244"/>
+        <source>Program code</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ColorChooserDialog</name>
+    <message>
+        <location filename="../sailfish/qml/pages/ColorChooserDialog.qml" line="45"/>
+        <source>Choose color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/ColorChooserDialog.qml" line="57"/>
+        <source>#</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ColorsSettingsPage</name>
+    <message>
+        <location filename="../sailfish/qml/pages/ColorsSettingsPage.qml" line="44"/>
+        <source>Color settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/ColorsSettingsPage.qml" line="89"/>
+        <source>Base text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/ColorsSettingsPage.qml" line="90"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/ColorsSettingsPage.qml" line="91"/>
+        <source>Internal anchor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/ColorsSettingsPage.qml" line="92"/>
+        <source>Note anchor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/ColorsSettingsPage.qml" line="93"/>
+        <source>External anchor</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FontSettingsPage</name>
+    <message>
+        <location filename="../sailfish/qml/pages/FontSettingsPage.qml" line="33"/>
+        <source>Font settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FormatTypesPage</name>
+    <message>
+        <location filename="../sailfish/qml/pages/FormatTypesPage.qml" line="43"/>
+        <source>Format types</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LibraryListView</name>
+    <message>
+        <location filename="../sailfish/qml/pages/LibraryListView.qml" line="48"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LocalView</name>
+    <message>
+        <location filename="../sailfish/qml/pages/LocalView.qml" line="33"/>
+        <source>Library</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OpdsBookModel</name>
+    <message>
+        <location filename="../lib/models/opdsparser.h" line="106"/>
+        <source>Download book (fb2)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsListPage</name>
+    <message>
+        <location filename="../sailfish/qml/pages/SettingsListPage.qml" line="38"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/SettingsListPage.qml" line="80"/>
+        <source>Texts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/SettingsListPage.qml" line="80"/>
+        <source>Font size, borders, etc</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../app/qml/android/qml/main.qml" line="18"/>
+        <source>Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/android/qml/main.qml" line="20"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/android/qml/main.qml" line="24"/>
+        <location filename="../app/qml/android/qml/main.qml" line="89"/>
+        <source>Collection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/android/qml/main.qml" line="27"/>
+        <source>Bookmarks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/android/qml/main.qml" line="82"/>
+        <source>CuteReader</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
